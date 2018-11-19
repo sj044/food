01baa75a3b826c9ed566cfd80cf62e6146dd0592
sj044 <45170312+sj044@users.noreply.github.com>
2018-11-19 13:48:11 +0100

    Add files via upload

---
+++ add.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+</head>
+
+<form action="index.php" method="get">
+Name: <input type = „text“ name="Name"><br>
+ <select name="Begrüßung">
+     <option value="Guten Morgen ">Guten Morgen </option>
+     <option value="Mahlzeit ">Mahlzeit </option>
+     <option value="Guten Abend ">Guten Abend </option>
+     <option value="Gute Nacht ">Gute Nacht </option>
+</select>
+<br>
+I Say YES! to ...
+<br>
+<select name="Unfug 0">
+     <option value="Burger ">Burger </option>
+     <option value="Hot Dog ">Hot Dog </option>
+     <option value="Spaghetti ">Spaghetti </option>
+     <option value="Donuts ">Donuts </option>
+</select>
+<select name="Unfug 1">
+     <option value="Brokolli ">Brokolli </option>
+     <option value="Möhre ">Möhre </option>
+     <option value="Staudensellerie ">Staudensellerie </option>
+     <option value="Böhnchen ">Böhnchen </option>
+</select>
+<select name="Unfug 1">
+     <option value="Cola ">Cola </option>
+     <option value="Milch ">Milch </option>
+     <option value="Wodka ">Wodka </option>
+     <option value="O-Saft ">O-Saft </option>
+</select>
+
+<br>
+<input type = "submit" value= "los geht's!">
+</form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+
+	<?php
+	echo $_GET ['Begrüßung'];
+	echo $_GET ['Name'];
+
+	?>
+</body>
+</html>
