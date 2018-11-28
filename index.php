7d18204e99105268031aee9ff3403e270a63b354
sj044 <45170312+sj044@users.noreply.github.com>
2018-11-28 10:45:33 +0100

    Add files via upload
    Versuch if-else-Bedingung einzubauen, um den Fall dass kein Name eingetragen ist abzudecken, hat nicht funktioniert

--- index.php
+++ index.php
@@ -1,11 +1,61 @@
+<?php
+$text = urldecode($_REQUEST['Unfug_0']) . " " . urldecode($_REQUEST['Unfug_1']) . " " . urldecode($_REQUEST['Unfug_2']) . "\n";
+
+if (empty($_POST['Name']))
+{
+
+echo "Unbekannter";
+echo file_put_contents("text.txt", $text, FILE_APPEND); 
+}
+
+else {
+
+echo "$_POST['Name']";
+echo file_put_contents("text.txt", $text, FILE_APPEND);
+}
+?>
+
+
+<!-- urldecode — Dekodiert eine URL-kodierte Zeichenkette
+-->
+
+<!-- PHP stellt dazu die Arrays $_GET (alle per GET übertragenen
+Daten), $_POST (alle per POST übertragenen Daten) und
+$_REQUEST (alle übertragenen Daten) zur Verfügung. 
+-->
+
+<!-- file_put_contents($filename, $text, FILE_APPEND)
+Schreibt $text in die Datei $filename. Wenn die Datei nicht
+existiert, wird sie angelegt. FILE_APPEND sorgt dafür, dass der
+Inhalt an die Datei gehängt und nicht nur der alte Inhalt
+überschrieben wird. 
+-->
+
 <!DOCTYPE HTML>
-<html>
+<html lang="DE"> <!--Angabe, dass deutsche Sprache -->
+
 <body>
 
 	<?php
 	echo $_GET ['Begrüßung'];
-	echo $_GET ['Name'];
-
 	?>
+	&#160;
+	<?php
+	echo $_GET ['Name'];
+	?>
+	, you said yes to 
+	<?php
+	echo $_GET ['Unfug_0'];
+	?> 
+	,
+	<?php
+	echo $_GET ['Unfug_1'];
+	?>
+	&
+	<?php
+	echo $_GET ['Unfug_2'];
+	?>
+	!
+
 </body>
 </html>
